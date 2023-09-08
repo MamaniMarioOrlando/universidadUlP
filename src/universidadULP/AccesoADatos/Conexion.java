4c81416741cdcde6326c3e2a77b5cb3aaff18da4
MamaniMarioOrlando <46626380+MamaniMarioOrlando@users.noreply.github.com>
2023-09-08 16:42:46 -0300

    add code to conexio

--- src/universidadULP/AccesoADatos/Conexion.java
+++ src/universidadULP/AccesoADatos/Conexion.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package universidadULP.AccesoADatos;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 /**
  *
  * @author MaRio
  */
 public class Conexion {
+    
+    private static final String URL = "jdbc:mariadb://localhost:3306/";
+    private static final String DB = "db_ulp";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    
+    private static Connection connection;
+    
+    private Conexion(){}
+    
+    public static Connection getConnection(){
+    
+        if(connection == null){
+        
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                
+                connection = DriverManager
+                        .getConnection(URL+DB, USER, PASSWORD);
+                System.out.println("Conectado a la base de datos!");
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar los Drivers" + ex.getMessage() );
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectarce a la Base de datos" + ex.getMessage() );
+            }             
+        }
+        return connection;  
+    }
     
 }
