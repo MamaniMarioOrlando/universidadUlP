1192e4c8152fc2957a270e7b37de70f1d8d793d4
LoreGcamp <141676335+LoreGcamp@users.noreply.github.com>
2023-09-08 16:56:32 -0300

    Clases Entidades

--- src/universidadULP/Entidades/Inscripcion.java
+++ src/universidadULP/Entidades/Inscripcion.java
@@ -1,13 +1,69 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package universidadULP.Entidades;
 
 /**
  *
- * @author MaRio
+ * @author MaRio y Lore
  */
 public class Inscripcion {
+    private int idInscripcion;
+    private int nota;
+    private Alumno alumno;
+    private Materia materia;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(int idInscripcion, int nota, Alumno alumno, Materia materia) {
+        this.idInscripcion = idInscripcion;
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public Inscripcion(int nota, Alumno alumno, Materia materia) {
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public int getIdInscripcion() {
+        return idInscripcion;
+    }
+
+    public void setIdInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    @Override
+    public String toString() {
+        String insc=idInscripcion+" "+alumno.getApellido()+", "+alumno.getNombre()+" "+materia.getNombre();
+        return insc;
+    }
+    
     
 }
