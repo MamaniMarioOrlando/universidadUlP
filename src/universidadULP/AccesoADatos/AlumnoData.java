ce48f862001e6295379143e7918f7407d37d9235
LoreGcamp <141676335+LoreGcamp@users.noreply.github.com>
2023-09-08 17:51:26 -0300

    Clase alumnoData
    Método Guardar

--- src/universidadULP/AccesoADatos/AlumnoData.java
+++ src/universidadULP/AccesoADatos/AlumnoData.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package universidadULP.AccesoADatos;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import universidadULP.Entidades.Alumno;
 
 /**
  *
- * @author MaRio
+ * @author MaRio y Lore
  */
 public class AlumnoData {
+    private Connection connection =  null;
+    
+    public AlumnoData() {
+        connection = Conexion.getConnection();
+    }
+    
+    public void guargarAlumno(Alumno alumno){
+       
+        try {
+            String sql = "INSERT INTO alumno (dni, apellido, nombre, fechaNacimiento, estado)"
+                + "VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS); //Retorna una lista de las claves generadas
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNacimiento()));
+            ps.setBoolean(5, alumno.isEstado());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()){
+                alumno.setIdAlumno(rs.getInt("idAlumno"));
+                JOptionPane.showMessageDialog(null, "Alumno guardado con exito");
+            }
+            ps.close(); //Cerramos el objeto creado por PreparedStatement
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumno");
+        }
+        
+    }
+    
     
 }
