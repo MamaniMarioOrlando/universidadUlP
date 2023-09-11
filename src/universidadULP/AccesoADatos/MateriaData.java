f5c9a4d86f21729be12204a046437aaa75a68ee5
MamaniMarioOrlando <46626380+MamaniMarioOrlando@users.noreply.github.com>
2023-09-11 17:30:35 -0300

    Add methods to the class MateriaData

--- src/universidadULP/AccesoADatos/MateriaData.java
+++ src/universidadULP/AccesoADatos/MateriaData.java
@@ -4,10 +4,40 @@
  */
 package universidadULP.AccesoADatos;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import universidadULP.Entidades.Materia;
+
 /**
  *
  * @author MaRio
  */
 public class MateriaData {
+    
+    Connection connection = Conexion.getConnection();
+
+    public MateriaData() {
+    }
+    
+    public void guardarMateria(Materia materia){
+        
+
+        try {
+            String sqlInsert = "INSERT INTO materia(nombre, año, estado)"
+                            + "VALUES(?,?,?)";
+            
+            PreparedStatement preparedStatement = connection.prepareCall(sqlInsert);
+            
+            preparedStatement.setString(1, materia.getNombre());
+            preparedStatement.setInt(1, materia.getAnio());
+            preparedStatement.setBoolean(3, materia.isEstado());
+            
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(MateriaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
