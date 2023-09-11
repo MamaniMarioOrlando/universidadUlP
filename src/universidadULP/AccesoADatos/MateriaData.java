7a37b1bc4ab8e4c99d1a9af0e8d902e1518fd7f6
MamaniMarioOrlando <46626380+MamaniMarioOrlando@users.noreply.github.com>
2023-09-11 17:53:29 -0300

    Add method of the class MateriaData

--- src/universidadULP/AccesoADatos/MateriaData.java
+++ src/universidadULP/AccesoADatos/MateriaData.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package universidadULP.AccesoADatos;
 
 import java.sql.*;
@@ -14,7 +10,7 @@ import universidadULP.Entidades.Materia;
 
 /**
  *
- * @author MaRio y Lore 
+ * @author MaRio y Lore
  */
 public class MateriaData {
     
